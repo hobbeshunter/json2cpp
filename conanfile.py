eb0fc7c3a580bf390a2d1cda465af0ea573beef8
Martin Wudenka <Martin.Wudenka@gmx.de>
2023-07-30 10:35:15 +0200

    feature: replace build system by conan 2 based one

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,13 @@
-import os
+import os, re
 
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
-from conan.tools.files import copy
+from conan.tools.files import copy, load
+from conan.tools.build import check_min_cppstd
 
 
 class json2cppRecipe(ConanFile):
     name = "json2cpp"
-    version = "0.0.1"
     package_type = "header-library"
 
     # Optional metadata
@@ -21,8 +21,30 @@ class json2cppRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
 
     # Sources are located in the same place as this recipe, copy them to the recipe
-    exports_sources = "CMakeLists.txt", "src/*", "include/*", "test/*", "examples/*", "cmake/*", \
-        "ProjectOptions.cmake", "Dependencies.cmake"
+    exports_sources = "CMakeLists.txt", "src/*", "include/*", "test/*", "examples/*", "cmake/*"
+
+    @property
+    def _min_cppstd(self):
+        return "20"
+
+    @property
+    def _build_tests(self):
+        return bool(self.conf.get("user.build:tests", default=False))
+
+    @property
+    def _build_large_tests(self):
+        return self._build_tests and bool(self.conf.get("user.build:large_tests", default=False))
+
+    def set_version(self):
+        content = load(
+            self,
+            os.path.join(
+                self.recipe_folder,
+                "src/CMakeLists.txt"))
+        version = re.search(
+            r"project\([^\)]+VERSION (\d+\.\d+\.\d+)[^\)]*\)", content
+        ).group(1)
+        self.version = version.strip()
 
     def layout(self):
         cmake_layout(self)
@@ -31,28 +53,30 @@ class json2cppRecipe(ConanFile):
         self.requires("valijson/1.0", transitive_headers=True, visible=True)
         self.requires("spdlog/1.11.0", visible=False)
         self.requires("nlohmann_json/3.11.2", visible=False)
-        self.requires("fmt/9.1.0", visible=False)
+        self.requires("fmt/10.0.0", visible=False)
         self.requires("cli11/2.3.2", visible=False)
-        self.requires("ftxui/4.1.0", visible=False)
         # self.requires("ftxui/4.1.1", visible=False)
 
     def build_requirements(self):
         self.test_requires("catch2/3.3.2")
 
+    def validate(self):
+        check_min_cppstd(self, self._min_cppstd)
+
     def generate(self):
         deps = CMakeDeps(self)
         deps.generate()
         tc = CMakeToolchain(self)
-        tc.variables["json2cpp_ENABLE_LARGE_TESTS"] = "OFF"
-        tc.variables["json2cpp_ENABLE_CLANG_TIDY"] = "OFF"
-        tc.variables["json2cpp_ENABLE_CPPCHECK"] = "OFF"
-        tc.variables["json2cpp_ENABLE_COVERAGE"] = "OFF"
+        tc.variables["json2cpp_ENABLE_LARGE_TESTS"] = self._build_large_tests
+        # tc.variables["json2cpp_ENABLE_COVERAGE"] = "OFF"
         tc.generate()
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure()
+        cmake.configure(build_script_folder=None if self._build_tests else "src")
         cmake.build()
+        if self._build_tests:
+            cmake.test()
 
     def package(self):
         copy(
@@ -71,4 +95,4 @@ class json2cppRecipe(ConanFile):
         self.info.settings.rm_safe("build_type")
 
     def package_info(self):
-        self.cpp_info.components["json2cpp_headers"].set_property("cmake_target_name", "json2cpp::json2cpp_headers")
+        self.cpp_info.components["json2cpp_libjson2cpp"].set_property("cmake_target_name", "json2cpp::libjson2cpp")
